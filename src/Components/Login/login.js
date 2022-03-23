8b04f776ac4d8b305e5cf4cbde255c6a0e2f40f5
matdexir <matthieudesir21@gmail.com>
2022-03-23 10:26:18 +0800

    Cleanup of the environment

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,14 +1,46 @@
 import  styled from "styled-components";
 
 const Button = styled.button`
-	background: blue;
-	size: 2em;
+	background: transparent;
+  border-radius: 3px;
+  border: 2px solid blue;
+  color: blue;
+  margin: 0 1em;
+  padding: 0.25em 1em
+
+	
 `
 
-const Login = () => {
+const CenteredDiv = styled.div`
+	margin-left: 20vw;
+	margin-right: 20vw;
+	margin-top: 10vh;
+	margin-bottom: 10vh;
+	padding: 5vw;
+	border: 2px solid blue;
+`
+
+const Input = styled.input`
+	size: 5vw;
+`
+
+const Space = styled.div`
+	margin: 5%;
+	padding: 2%;
+`
+
+const Login = ({user, setUser, password, setPassword}) => {
 	return(
 		<form>
+			<CenteredDiv>
+				<Space>
+				<Input placeholder="Username" onChange={(e)=> setUser(e.value)}/>
+			</Space>
+					<Space>
+				<Input placeholder="Password" onChange={(e)=> setPassword(e.value)}/>
+			</Space>
 			<Button>Hello world</Button>
+		</CenteredDiv>
 		</form>
 	)
 }
