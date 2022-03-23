e3f7a1fb792ab4149a22a95a30c6a998f4cdc189
matdexir <matthieudesir21@gmail.com>
2022-03-23 17:22:50 +0800

    File restructuring

---
+++ frontend/src/Components/Styled/styled.js
@@ -0,0 +1,54 @@
+import styled, { css } from "styled-components";
+
+const Button = styled.button`
+    background: transparent;
+    border-radius: 5px;
+    border: 2px solid blue;
+    color: blue;
+    padding: 0.25em 1em;
+    width: 30vw;
+		&:hover {
+			background-color: blue;
+			opacity: 0.9;
+		}
+
+    ${(props) =>
+        props.primary &&
+        css`
+            background: blue;
+            color: white;
+						&:hover {
+							opacity: 0.6;
+						}
+        `}
+`;
+
+const CenteredDiv = styled.div`
+    background-color: #1f1f1f;
+		border-radius: 25px;
+    margin-left: 10vw;
+    margin-right: 10vw;
+    margin-top: 20vh;
+    margin-bottom: 20vh;
+    padding: 5vw;
+    border: 2px solid blue;
+`;
+
+const Input = styled.input`
+    width: 30vw;
+`;
+
+const Space = styled.div`
+    background-color: #1f1f1f;
+    margin: 5%;
+    text-align: center;
+`;
+
+const Title = styled.h1`
+    background-color: #1f1f1f;
+    font-size: 1.75rem;
+    color: black;
+    text-align: center;
+`;
+
+export { Title, Input, Space, CenteredDiv, Button };
---
+++ frontend/src/axios.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const instance = axios.create({ baseURL: "http://localhost:4000/" });
+
+const Login = async () => {
+	// TODO
+}
+
+const PauseTime = async () => {
+	// TODO
+}
+
+const Reset = async () => {
+	// TODO
+}
+
+export {Login, PauseTime, Reset};
